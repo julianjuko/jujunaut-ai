05eed9f2bcbdfddcd0cce40e1eaa42b5cc625eec
julianjuko <julian.anning@gmail.com>
2024-04-29 00:29:35 +1000

    feat: blockquote responses

--- main.ts
+++ main.ts
@@ -19,6 +19,7 @@ interface JujunautAiSettings {
 	temperature: number;
 	topP: number;
 	useBlockquote: boolean;
+	blockquotePrefix: string;
 }
 
 export type Prompt = {
@@ -33,8 +34,19 @@ const DEFAULT_SETTINGS: JujunautAiSettings = {
 	presencePenalty: 0,
 	temperature: 1,
 	topP: 0.95,
-	useBlockquote: false, // Default to false
+	useBlockquote: false,
+	blockquotePrefix: "AI",
 };
+
+const blockQuotePrompts: Prompt[] = [
+	{
+		role: "system",
+		content:
+			"Do not format your responses in blockquotes. The plugin will handle that for you.",
+	},
+];
+
+const hardCodedSystemPrompts: Prompt[] = [...blockQuotePrompts];
 
 export default class JujunautAiPlugin extends Plugin {
 	settings: JujunautAiSettings;
@@ -138,24 +150,50 @@ export default class JujunautAiPlugin extends Plugin {
 
 	// Update the resolvePath method to handle different types of paths
 	resolvePath(filePath: string): string {
-		// Handle home directory paths
+		// Normalize backslashes to forward slashes for cross-platform compatibility
+		filePath = filePath.replace(/\\/g, "/");
+
+		// Handle absolute paths for Linux or Unix-like systems
+		if (filePath.startsWith("/")) {
+			return filePath; // It's already an absolute path
+		}
+
+		// Handle relative paths including ".." which navigate up the directory tree
+		if (filePath.startsWith(".") || filePath.includes("..")) {
+			filePath = path.resolve(this.app.vault.getRoot().path, filePath);
+			return filePath;
+		}
+
+		// Handle Windows absolute paths (e.g., C:\, D:\)
+		if (/^[a-zA-Z]:\\/.test(filePath)) {
+			// Convert Windows path to a more neutral format, just in case
+			filePath = path.join(...filePath.split("\\"));
+			return filePath;
+		}
+
+		// Handle Windows network paths (e.g., \\Server\Share)
+		if (filePath.startsWith("\\\\")) {
+			filePath = `\\\\${path.join(...filePath.slice(2).split("\\"))}`;
+			return filePath;
+		}
+
+		// Handle Android-specific paths (assuming the app handles Android's file system correctly)
+		// Example: /storage/emulated/0/ or /sdcard/
+		if (
+			filePath.includes("/storage/emulated/") ||
+			filePath.includes("/sdcard/")
+		) {
+			return filePath; // Android absolute paths
+		}
+
+		// Handle home directory paths on Unix-like systems
 		if (filePath.startsWith("~")) {
-			filePath = path.join(
-				process.env.HOME || process.env.USERPROFILE || "",
-				filePath.slice(1)
-			);
+			filePath = path.join(process.env.HOME || "", filePath.slice(1));
+			return filePath;
 		}
 
-		// Resolve relative paths
-		if (!path.isAbsolute(filePath)) {
-			filePath = path.resolve(
-				//@ts-ignore
-				this.app.vault.adapter.basePath,
-				filePath
-			);
-		}
-
-		return filePath;
+		// Dfalseefault to resolving the path relative to the Obsidian vault's base path
+		return path.resolve(this.app.vault.getRoot().path, filePath);
 	}
 
 	async generateText(editor: Editor, view: MarkdownView) {
@@ -181,13 +219,19 @@ export default class JujunautAiPlugin extends Plugin {
 
 		await this.streamOpenAI(text, editor, systemPrompts);
 	}
+
 	async streamOpenAI(
 		prompt: string,
 		editor: Editor,
 		systemPrompts: Prompt[]
 	): Promise<void> {
 		const apiKey = this.settings.openAiApiKey;
-		const messages = [...systemPrompts, { role: "user", content: prompt }];
+		const messages = [
+			...hardCodedSystemPrompts,
+			...systemPrompts,
+			{ role: "user", content: prompt },
+		];
+		console.log(messages);
 
 		const response = await fetch(
 			"https://api.openai.com/v1/chat/completions",
@@ -221,11 +265,20 @@ export default class JujunautAiPlugin extends Plugin {
 
 		try {
 			if (this.settings.useBlockquote) {
-				editor.replaceSelection("> ");
+				editor.replaceSelection(
+					`> [!ai] ${
+						this.settings.blockquotePrefix.length
+							? this.settings.blockquotePrefix
+							: "AI"
+					}\n> `
+				);
 			}
-			while (this.isStreaming) {
+
+			let continueReading = true; // Flag to control loop
+			while (continueReading) {
 				const { done, value } = await this.streamReader.read();
 				if (done) {
+					continueReading = false; // Exit condition
 					break;
 				}
 
@@ -233,9 +286,11 @@ export default class JujunautAiPlugin extends Plugin {
 				chunk = chunk.replace(/^data: /gm, "");
 				buffer += chunk;
 
-				while (true) {
+				let processBuffer = true; // Flag to process buffer content
+				while (processBuffer) {
 					const endOfObject = buffer.indexOf("\n");
 					if (endOfObject === -1) {
+						processBuffer = false; // Exit condition for inner loop
 						break;
 					}
 
@@ -390,6 +445,19 @@ export default class JujunautAiPlugin extends Plugin {
 					.setValue(this.settings.useBlockquote)
 					.onChange(async (value) => {
 						this.settings.useBlockquote = value;
+						await this.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Blockquote Prefix")
+			.setDesc("Title of the generated blockquote.")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your API key")
+					.setValue(this.settings.blockquotePrefix)
+					.onChange(async (value) => {
+						this.settings.blockquotePrefix = value;
 						await this.saveSettings();
 					})
 			);
