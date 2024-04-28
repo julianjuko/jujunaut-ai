a98fd22c5de473ee9a24add4e28a110f94ae7253
julianjuko <julian.anning@gmail.com>
2024-04-29 00:46:47 +1000

    feat: update desktop app icons

--- main.ts
+++ main.ts
@@ -73,7 +73,7 @@ export default class JujunautAiPlugin extends Plugin {
 		});
 
 		const ribbonIconEl = this.addRibbonIcon(
-			"star",
+			"bot",
 			"Jujunaut AI",
 			async (evt: MouseEvent) => {
 				const activeView =
